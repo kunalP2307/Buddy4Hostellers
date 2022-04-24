fde22320fb33d0ae91da4903f71a51aeb74bd5ff
kunalP2307 <kunalpatil2307@gmail.com>
2022-04-24 17:36:59 +0530

    Added Implementation for Spinners in Registration Module

---
+++ app/src/main/java/com/example/buddy4hostellers/data/CollegeDetails.java
@@ -0,0 +1,28 @@
+package com.example.buddy4hostellers.data;
+
+public class CollegeList {
+
+    public static final String[] colleges = {
+            "DR. D. Y. PATIL INSTITUTE OF TECHNOLOGY Pimpri",
+            "DR. D. Y. PATIL MEDICAL COLLEGE, HOSPITAL & RESEARCH CENTRE Pimpri",
+            "D Y PATIL COLLEGE OF ENGINEERING AKURDI, PUNE",
+            "MAHATMA PHULE MAHAVIDYALAYA , Pimpri",
+            "Government Polytechnic, Pune",
+            "College Of Engineering Pune",
+            "Marathwada Mitra Mandal's College of Pharmacy Kalewadi",
+            "PIMPRI CHINCHWAD COLLEGE OF ENGINEERING Akurdi",
+            "International Institute of Information Technology Hinjwadi"
+    };
+
+    public static final String[] courses = {
+            "Select Course",
+            "Bachelor of Engineering",
+            "Bachelor of Commerce",
+            "Bachelor of Arts",
+            "Diploma in Engineering",
+            "Masters of Engineering",
+            "MBA",
+            "Other"
+    };
+
+}
