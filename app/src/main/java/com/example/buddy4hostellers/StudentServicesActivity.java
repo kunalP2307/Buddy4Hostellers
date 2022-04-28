199f6d817ce2dabe4b554a037432b98a080b847d
kunalP2307 <kunalpatil2307@gmail.com>
2022-04-28 22:03:34 +0530

    Added login Sign up module for both both users

---
+++ app/src/main/java/com/example/buddy4hostellers/StudentServicesActivity.java
@@ -0,0 +1,14 @@
+package com.example.buddy4hostellers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentServicesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_services);
+    }
+}
---
+++ app/src/main/java/com/example/buddy4hostellers/data/ServiceProvider.java
@@ -0,0 +1,2 @@
+package com.example.buddy4hostellers.data;public class ServiceProvider {
+}
