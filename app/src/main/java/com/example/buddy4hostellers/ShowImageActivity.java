084c202617bf5433bcc0f9b507a80fffc67e5205
kunalP2307 <kunalpatil2307@gmail.com>
2022-05-03 16:12:55 +0530

    Added Java Classes for Hostel/PG

--- app/src/main/java/com/example/buddy4hostellers/ShowImageActivity.java
+++ app/src/main/java/com/example/buddy4hostellers/ShowImageActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SelectLocationActivity extends AppCompatActivity {
+public class ShowImageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_location);
+        setContentView(R.layout.activity_show_image);
     }
 }
---
+++ app/src/main/java/com/example/buddy4hostellers/data/Amenities.java
@@ -0,0 +1,4 @@
+package com.example.buddy4hostellers.data;
+
+public class Amenities {
+}
